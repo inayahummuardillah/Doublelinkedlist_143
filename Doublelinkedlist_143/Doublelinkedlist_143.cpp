da0094f197047b6102d983719aa3e4508bdaefe3
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-25 14:55:25 +0630

    Add project files.

---
+++ Doublelinkedlist_143/Doublelinkedlist_143.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node {
+public:
+	int noMhs;
+	string name;
+	node* next;
+	node* prev;
+};
+
+class DoubleLinkedList {
+private:
+	node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+};
